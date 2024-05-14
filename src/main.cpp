e73cd65bca32b6403ca6cbb94bb1b672df2b193f
Max Schneider <ms.matix@googlemail.com>
2024-05-14 15:47:47 +0200

    chore(googletest): update version for neotest plugin

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
-#include "modul1/foo.h"
-#include "modul2/bar.h"
 #include <etl/vector.h>
+
 #include <iostream>
 
+#include "modul1/foo.h"
+#include "modul2/bar.h"
+
 int main() {
-  Foo foo;
-  foo.doSomething();
+    Foo foo;
+    foo.doSomething();
 
-  Bar bar;
-  bar.doSomething();
+    Bar bar;
+    bar.doSomething();
 
-  std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
 
-  etl::vector<int, 10> myVector{};
-  return 0;
+    etl::vector<int, 10> myVector{};
+    return 0;
 }
--- tst/test_foo.cpp
+++ tst/test_foo.cpp
@@ -1,17 +1,18 @@
-#include "../src/modul1/foo.h"
 #include <gtest/gtest.h>
 
+#include "../src/modul1/foo.h"
+
 TEST(FooTest, HandlesZeroInput) {
-  Foo foo;
-  EXPECT_EQ(0, foo.add(0, 0));
+    Foo foo;
+    EXPECT_EQ(0, foo.add(0, 0));
 }
 
 TEST(FooTest, HandlesPositiveInput) {
-  Foo foo;
-  EXPECT_EQ(5, foo.add(2, 3));
+    Foo foo;
+    EXPECT_EQ(5, foo.add(2, 3));
 }
 
 TEST(FooTest, HandlesNegativeInput) {
-  Foo foo;
-  EXPECT_EQ(-1, foo.add(-3, 2));
+    Foo foo;
+    EXPECT_EQ(-1, foo.add(-3, 2));
 }
